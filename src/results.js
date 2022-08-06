8ab9d2fcd17456d7ac2eba847bfdffae85a8e75b
Nayoung-Gu <skdud000322@naver.com>
2022-08-06 22:29:36 +0900

    Design: Markup for description and links

--- src/results.js
+++ src/results.js
@@ -3,6 +3,7 @@ const perfumes = {
         brand: "Acqua di Parma",
         product: "피코 디 아말피 EDT",
         img: "images/LSharpF.PNG",
+        desc: "청량하고 깨끗한 지중해 풍경의 무화과 나무가 연상되는 향수입니다.",
         korLink:
             "https://perfumegraphy.com/product/%EC%95%84%EC%BF%A0%EC%95%84-%EB%94%94-%ED%8C%8C%EB%A5%B4%EB%A7%88-%ED%94%BC%EC%BD%94-%EB%94%94-%EC%95%84%EB%A7%90%ED%94%BC-%EC%98%A4-%EB%93%9C-%EB%9A%9C%EC%99%88%EB%A0%9B/1656/category/146/display/1/",
         engLink:
@@ -12,6 +13,7 @@ const perfumes = {
         brand: "Maison Francis Kurkdjian",
         product: "아쿠아 셀레스티아 EDT",
         img: "images/LSharpG.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%EB%A9%94%EC%A2%85-%ED%94%84%EB%9E%80%EC%8B%9C%EC%8A%A4-%EC%BB%A4%EC%A0%95-%EC%95%84%EC%BF%A0%EC%95%84-%EC%85%80%EB%A0%88%EC%8A%A4%ED%8B%B0%EC%95%84-%EC%98%A4-%EB%93%9C-%EB%9A%9C%EC%99%88%EB%A0%9B-70ml/1532/category/146/display/1/",
         engLink:
@@ -21,6 +23,7 @@ const perfumes = {
         brand: "Creed",
         product: "어벤투스 포허",
         img: "images/LSmoothF.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%EB%A9%94%EC%A2%85-%ED%94%84%EB%9E%80%EC%8B%9C%EC%8A%A4-%EC%BB%A4%EC%A0%95-%EC%95%84%EC%BF%A0%EC%95%84-%EC%85%80%EB%A0%88%EC%8A%A4%ED%8B%B0%EC%95%84-%EC%98%A4-%EB%93%9C-%EB%9A%9C%EC%99%88%EB%A0%9B-70ml/1532/category/146/display/1/",
         engLink:
@@ -30,6 +33,7 @@ const perfumes = {
         brand: "Maison Margiela",
         product: "언더 더 레몬트리",
         img: "images/LSmoothG.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%EB%A9%94%EC%A2%85%EB%A7%88%EB%A5%B4%EC%A7%80%EC%97%98%EB%9D%BC-%EC%96%B8%EB%8D%94-%EB%8D%94-%EB%A0%88%EB%AA%AC%ED%8A%B8%EB%A6%AC/1662/category/146/display/1/",
         engLink:
@@ -39,6 +43,7 @@ const perfumes = {
         brand: "Loewe",
         product: "001 맨 EDT",
         img: "images/DSharpF.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%EB%A9%94%EC%A2%85%EB%A7%88%EB%A5%B4%EC%A7%80%EC%97%98%EB%9D%BC-%EC%96%B8%EB%8D%94-%EB%8D%94-%EB%A0%88%EB%AA%AC%ED%8A%B8%EB%A6%AC/1662/category/146/display/1/",
         engLink:
@@ -48,6 +53,7 @@ const perfumes = {
         brand: "Bastille",
         product: "홀스 피스테 EDP",
         img: "images/DSharpG.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%EB%B0%94%EC%8A%A4%ED%8B%B8-%ED%99%80%EC%8A%A4-%ED%94%BC%EC%8A%A4%ED%85%8C-%EC%98%A4-%EB%93%9C-%ED%8D%BC%ED%93%B8-50ml/2024/category/146/display/1/",
         engLink:
@@ -57,6 +63,7 @@ const perfumes = {
         brand: "Prada",
         product: "레스 인퓨전 디 아이리스 세더 EDP",
         img: "images/DSmoothF.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%ED%94%84%EB%9D%BC%EB%8B%A4-%EB%A0%88%EC%8A%A4-%EC%9D%B8%ED%93%A8%EC%A0%84-%EB%94%94-%EC%95%84%EC%9D%B4%EB%A6%AC%EC%8A%A4-%EC%84%B8%EB%8D%94-%EC%98%A4-%EB%93%9C-%ED%8D%BC%ED%93%B8-100ml/1325/category/146/display/1/",
         engLink:
@@ -66,6 +73,7 @@ const perfumes = {
         brand: "Jo Malone",
         product: "인센스 앤 세드라 코롱 인텐스",
         img: "images/DSmoothG.PNG",
+        desc: "",
         korLink:
             "https://perfumegraphy.com/product/%EC%A1%B0%EB%A7%90%EB%A1%A0-%EC%9D%B8%EC%84%BC%EC%8A%A4-%EC%95%A4-%EC%84%B8%EB%93%9C%EB%9D%BC-%EC%BD%94%EB%A1%B1-%EC%9D%B8%ED%85%90%EC%8A%A4-50ml/2402/category/146/display/1/",
         engLink:
